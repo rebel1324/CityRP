f4aa60f52bc4c8f6b862322cc7a61ff35565fb19
Kyu Yeon Lee <rebel1324@gmail.com>
2017-07-19 00:42:12 +0900

    Added FPP

---
+++ addons/Addon_FPPNutscript/lua/fpp/sh_cppi.lua
@@ -0,0 +1,106 @@
+CPPI = CPPI or {}
+CPPI.CPPI_DEFER = 102112 --\102\112 = fp
+CPPI.CPPI_NOTIMPLEMENTED = 7080 --\70\80 = FP
+
+function CPPI:GetName()
+	return "Falco's prop protection"
+end
+
+function CPPI:GetVersion()
+	return "universal.1"
+end
+
+function CPPI:GetInterfaceVersion()
+	return 1.3
+end
+
+function CPPI:GetNameFromUID(uid)
+	return CPPI.CPPI_NOTIMPLEMENTED
+end
+
+local PLAYER = FindMetaTable("Player")
+function PLAYER:CPPIGetFriends()
+	if not self.Buddies then return CPPI.CPPI_DEFER end
+	local FriendsTable = {}
+
+	for k,v in pairs(self.Buddies) do
+		if not table.HasValue(v, true) then continue end -- not buddies in anything
+		table.insert(FriendsTable, k)
+	end
+
+	return FriendsTable
+end
+
+local ENTITY = FindMetaTable("Entity")
+function ENTITY:CPPIGetOwner()
+	local Owner = FPP.entGetOwner(self)
+	if not IsValid(Owner) or not Owner:IsPlayer() then return SERVER and Owner or nil, self.FPPOwnerID end
+	return Owner, Owner:UniqueID()
+end
+
+if SERVER then
+	function ENTITY:CPPISetOwner(ply)
+		local valid = IsValid(ply) and ply:IsPlayer()
+		local steamId = valid and ply:SteamID() or nil
+		local canSetOwner = hook.Run("CPPIAssignOwnership", ply, self, valid and ply:UniqueID() or ply)
+
+		if canSetOwner == false then return false end
+		ply = canSetOwner ~= nil and canSetOwner ~= true and canSetOwner or ply
+		self.FPPOwner = ply
+		self.FPPOwnerID = steamId
+
+		self.FPPOwnerChanged = true
+		FPP.recalculateCanTouch(player.GetAll(), {self})
+		self.FPPOwnerChanged = nil
+
+		return true
+	end
+
+	function ENTITY:CPPISetOwnerUID(UID)
+		local ply = UID and player.GetByUniqueID(tostring(UID)) or nil
+		if UID and not IsValid(ply) then return false end
+		return self:CPPISetOwner(ply)
+	end
+
+	function ENTITY:CPPICanTool(ply, tool)
+		local Value = FPP.Protect.CanTool(ply, nil, tool, self)
+		if Value ~= false and Value ~= true then Value = true end
+		return Value
+	end
+
+	function ENTITY:CPPICanPhysgun(ply)
+		return FPP.plyCanTouchEnt(ply, self, "Physgun")
+	end
+
+	function ENTITY:CPPICanPickup(ply)
+		return FPP.plyCanTouchEnt(ply, self, "Gravgun")
+	end
+
+	function ENTITY:CPPICanPunt(ply)
+		return FPP.plyCanTouchEnt(ply, self, "Gravgun")
+	end
+
+	function ENTITY:CPPICanUse(ply)
+		return FPP.plyCanTouchEnt(ply, self, "PlayerUse")
+	end
+
+	function ENTITY:CPPICanDamage(ply)
+		return FPP.plyCanTouchEnt(ply,  self, "EntityDamage")
+	end
+
+	function ENTITY:CPPIDrive(ply)
+		local Value = FPP.Protect.CanDrive(ply, self)
+		if Value ~= false and Value ~= true then Value = true end
+		return Value
+	end
+
+	function ENTITY:CPPICanProperty(ply, property)
+		local Value = FPP.Protect.CanProperty(ply, property, self)
+		if Value ~= false and Value ~= true then Value = true end
+		return Value
+	end
+
+	function ENTITY:CPPICanEditVariable(ply, key, val, editTbl)
+		return self:CPPICanProperty(ply, "editentity")
+	end
+end
---
+++ addons/Addon_FPPNutscript/lua/fpp/sh_settings.lua
@@ -0,0 +1,135 @@
+-- These are the default settings. Don't mind changing these.
+FPP = FPP or {}
+
+-- Don't reset the settings when they're already there
+if FPP.Settings then
+	return
+end
+
+FPP.Settings = {}
+FPP.Settings.FPP_PHYSGUN1 = {
+	toggle = 1,
+	adminall = 1,
+	worldprops = 0,
+	adminworldprops = 1,
+	canblocked = 0,
+	admincanblocked = 0,
+	shownocross = 1,
+	checkconstrained = 1,
+	reloadprotection = 1,
+	iswhitelist = 0}
+FPP.Settings.FPP_GRAVGUN1 = {
+	toggle = 1,
+	adminall = 1,
+	worldprops = 1,
+	adminworldprops = 1,
+	canblocked = 0,
+	admincanblocked = 0,
+	shownocross = 1,
+	checkconstrained = 1,
+	noshooting = 1,
+	iswhitelist = 0}
+FPP.Settings.FPP_TOOLGUN1 = {
+	toggle = 1,
+	adminall = 1,
+	worldprops = 1,
+	adminworldprops = 1,
+	canblocked = 0,
+	admincanblocked = 0,
+	shownocross = 1,
+	checkconstrained = 1,
+	iswhitelist = 0,
+
+	duplicatorprotect = 1,
+	duplicatenoweapons = 1,
+	spawniswhitelist = 0,
+	spawnadmincanweapon = 0,
+	spawnadmincanblocked = 0}
+FPP.Settings.FPP_PLAYERUSE1 = {
+	toggle = 0,
+	adminall = 1,
+	worldprops = 1,
+	adminworldprops = 1,
+	canblocked = 0,
+	admincanblocked = 1,
+	shownocross = 1,
+	checkconstrained = 0,
+	iswhitelist = 0}
+FPP.Settings.FPP_ENTITYDAMAGE1 = {
+	toggle = 1,
+	protectpropdamage = 1,
+	adminall = 1,
+	worldprops = 1,
+	adminworldprops = 1,
+	canblocked = 0,
+	admincanblocked = 0,
+	shownocross = 1,
+	checkconstrained = 0,
+	iswhitelist = 0}
+FPP.Settings.FPP_GLOBALSETTINGS1 = {
+	freezedisconnected = 0,
+	cleanupdisconnected = 1,
+	cleanupdisconnectedtime = 120,
+	cleanupadmin = 1,
+	antie2minge = 1}
+FPP.Settings.FPP_ANTISPAM1 = {
+	toggle = 1,
+	antispawninprop = 0,
+	bigpropantispam = 1,
+	bigpropsize = 5.85,
+	bigpropwait = 1.5,
+	smallpropdowngradecount = 3,
+	smallpropghostlimit = 2,
+	smallpropdenylimit = 6,
+	duplicatorlimit = 3
+}
+FPP.Settings.FPP_BLOCKMODELSETTINGS1 = {
+	toggle = 1,
+	propsonly = 0,
+	iswhitelist = 0
+}
+
+function FPP.ForAllSettings(fn)
+	-- Loop in sorted pairs for deterministic order
+	for kind, sets in SortedPairs(FPP.Settings) do
+		for setting, val in SortedPairs(sets) do
+			if fn(kind, setting, val) then break end
+		end
+	end
+end
+
+/*---------------------------------------------------------------------------
+CAMI
+Register the CAMI privilege
+---------------------------------------------------------------------------*/
+CAMI.RegisterPrivilege{
+	Name = "FPP_Settings",
+	MinAccess = "superadmin" -- By default only superadmins can change settings
+}
+
+CAMI.RegisterPrivilege{
+	Name = "FPP_Cleanup",
+	MinAccess = "admin"
+}
+
+
+CAMI.RegisterPrivilege{
+	Name = "FPP_TouchOtherPlayersProps",
+	MinAccess = "admin"
+}
+
+function FPP.calculatePlayerPrivilege(priv, callback)
+	local plys = player.GetAll()
+	local count = #plys
+
+	for _, ply in pairs(plys) do
+		local function onRes(b)
+			count = count - 1
+			ply.FPP_Privileges = ply.FPPPrivileges or {}
+			ply.FPP_Privileges[priv] = b
+
+			if count == 0 then callback() end
+		end
+		CAMI.PlayerHasAccess(ply, priv, onRes)
+	end
+end
