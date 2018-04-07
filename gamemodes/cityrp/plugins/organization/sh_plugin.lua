731fb907271ce7d52e2e0854dfe7017f97d48414
Kyu Yeon Lee <rebel1324@gmail.com>
2018-04-08 06:47:51 +0900

    Added Organization UI Interaction Parts.

--- gamemodes/cityrp/plugins/organization/sh_plugin.lua
+++ gamemodes/cityrp/plugins/organization/sh_plugin.lua
@@ -14,6 +14,8 @@ ORGANIZATION_AUTO_DELETE_TIME = 60*60*24*5 -- 5 days of inactivity will get your
 
 nut.util.include("meta/sh_character.lua")
 nut.util.include("meta/sh_organization.lua")
+nut.util.include("vgui/cl_orgmanager.lua")
+nut.util.include("vgui/cl_orgjoiner.lua")
 nut.util.include("sv_database.lua")
 
 if (SERVER) then
@@ -42,9 +44,37 @@ if (SERVER) then
     end
 
     function nut.org.delete(id)
-        nut.org.loaded[id]:unsync()
-        nut.org.loaded[id] = nil
-		nut.db.query("DELETE FROM nut_organization WHERE _id IN ("..id..")")
+        local org = nut.org.loaded[id]
+        
+        if (org) then
+            local affectedPlayers
+
+            for k, v in ipairs(player.GetAll()) do
+                local char = v:getChar()
+
+                if (char) then
+                    local charOrg = char:getOrganization()
+
+                    if (charOrg == id) then
+                        targetChar:setData("organization", nil, nil, player.GetAll())
+                        targetChar:setData("organizationRank", nil, nil, player.GetAll())
+
+                        table.insert(affectedPlayers, v)
+                    end
+                end
+            end
+
+            hook.Run("OnOranizationDeleted", org, affectedPlayers)
+
+            org:unsync()
+
+            nut.org.loaded[id] = nil
+            nut.db.query("DELETE FROM nut_organization WHERE _id IN ("..org.id..")")
+
+            return true
+        else
+            return false, "invalidOrg"
+        end
     end
 
     function nut.org.syncAll(recipient)
@@ -163,25 +193,59 @@ if (SERVER) then
             end)
         end)
     end
+
+    function PLUGIN:CanChangeOrganizationVariable(client, key, value)
+
+    end
+
+    function PLUGIN:CanCreateOrganization(client)
+        return true
+    end
+
+    function PLUGIN:OnCreateOrganization(client, organization)
+
+    end
+
+    function PLUGIN:PlayerCanJoinOrganization()
+
+    end
 end
 
 if (CLIENT) then
-    netstream.Hook("nutOrgCharSync", function(data)
-        for id, syncDat in pairs(data) do
-            local character = nut.char.loaded[id]
-            
-            if (character) then
-                character.vars.data = character.vars.data or {}
+    -- organization networkings
+    do
+        netstream.Hook("nutOrgCharSync", function(data)
+            for id, syncDat in pairs(data) do
+                local character = nut.char.loaded[id]
+                
+                if (character) then
+                    character.vars.data = character.vars.data or {}
 
-                character:getData()["organization"] = syncDat[1]
-                character:getData()["organizationRank"] = syncDat[2]
+                    character:getData()["organization"] = syncDat[1]
+                    character:getData()["organizationRank"] = syncDat[2]
+                end
             end
-        end
-    end)
-    --sync specific server organization data
-    netstream.Hook("nutOrgSyncAll", function(orgsData)
-        if (data) then
-            for id, data in pairs(orgsData) do
+        end)
+        --sync specific server organization data
+        netstream.Hook("nutOrgSyncAll", function(orgsData)
+            if (data) then
+                for id, data in pairs(orgsData) do
+                    local org = nut.org.new()
+
+                    for k, v in pairs(data) do
+                        org[k] = v
+                    end
+
+                    nut.org.loaded[id] = org
+                end
+            else
+                print("got org sync request but no data found")
+            end
+        end)
+
+        --sync specific server organization data
+        netstream.Hook("nutOrgSync", function(id, data)
+            if (data) then
                 local org = nut.org.new()
 
                 for k, v in pairs(data) do
@@ -189,69 +253,154 @@ if (CLIENT) then
                 end
 
                 nut.org.loaded[id] = org
+            else
+                print("got org sync request but no data found")
             end
-        else
-            print("got org sync request but no data found")
-        end
-    end)
+        end)
 
-    --sync specific server organization data
-    netstream.Hook("nutOrgSync", function(id, data)
-        if (data) then
-            local org = nut.org.new()
-
-            for k, v in pairs(data) do
-                org[k] = v
+        netstream.Hook("nutOrgRemove", function(id)
+            nut.org.loaded[id] = nil
+        end)
+        --sync 
+        netstream.Hook("nutOrgSyncValue", function(id, key, value)
+            if (nut.org.loaded[id]) then
+                nut.org.loaded[id][key] = value
             end
+        end)
 
-            nut.org.loaded[id] = org
-        else
-            print("got org sync request but no data found")
-        end
-    end)
+        netstream.Hook("nutOrgSyncData", function(id, key, value)
+            print(id, key, value)
+            if (nut.org.loaded[id] and nut.org.loaded[id].data) then
+                nut.org.loaded[id].data[key] = value
+            end
+        end)
+        
+        netstream.Hook("nutOrgSyncMember", function(id, rank, charID, isChange)
+            local org = nut.org.loaded[id]
 
-    netstream.Hook("nutOrgRemove", function(id)
-        nut.org.loaded[id] = nil
-    end)
-    --sync 
-    netstream.Hook("nutOrgSyncValue", function(id, key, value)
-        if (nut.org.loaded[id]) then
-            nut.org.loaded[id][key] = value
-        end
-    end)
-
-    netstream.Hook("nutOrgSyncData", function(id, key, value)
-        print(id, key, value)
-        if (nut.org.loaded[id] and nut.org.loaded[id].data) then
-            nut.org.loaded[id].data[key] = value
-        end
-    end)
-    
-    netstream.Hook("nutOrgSyncMember", function(id, rank, charID, isChange)
-        local org = nut.org.loaded[id]
-
-        if (org) then
-            if (isChange) then
-                for i = ORGANIZATION_MEMBER, ORGANIZATION_OWNER do
-                    if (org.members[i] and org.members[i][charID]) then
-                        org.members[i][charID] = nil
-                        break
+            if (org) then
+                if (isChange) then
+                    for i = ORGANIZATION_MEMBER, ORGANIZATION_OWNER do
+                        if (org.members[i] and org.members[i][charID]) then
+                            org.members[i][charID] = nil
+                            break
+                        end
                     end
                 end
+
+                local char = nut.char.loaded[charID]
+
+                org.members[rank] = org.members[rank] or {}
+                org.members[rank][charID] = char and char:getName() or true
             end
-
-            local char = nut.char.loaded[charID]
-
-            org.members[rank] = org.members[rank] or {}
-            org.members[rank][charID] = char and char:getName() or true
-        end
-    end)
-
-    netstream.Hook("nutOrgSyncOfflineMembers", function(id, data)
-        
-    end)
+        end)
+    end
 else
+    -- ui networkings
+    do
+        netstream.Hook("nutOrgCreate", function(client)
+            local char = client:getChar()
+            
+            if (char) then
+                if (hook.Run("CanCreateOrganization", client) == false) then return end
 
+                nut.org.create(function(orgObject)
+                    orgObject:setOwner(char)
+                    hook.Run("OnCreateOrganization", client, orgObject)
+                end)
+            end
+        end)
+
+        netstream.Hook("nutOrgJoin", function(client, orgID)
+            local char = client:getChar()
+            
+            if (char) then
+                local org = nut.org.loaded[orgID]
+
+                if (org) then
+                    local bool, reason = char:canJoinOrganization()
+
+                    if (bool != false) then
+                        org:addCharacter(char)
+                    else
+                        client:notifyLocalized(reason)
+                    end
+                else
+                    client:notifyLocalized("invalidOrg")
+                end
+            end
+        end)
+
+        netstream.Hook("nutOrgExit", function(client)
+            local char = client:getChar()
+            
+            if (char) then
+                local org = char:getOrganizationInfo()
+
+                if (org) then
+                    org:removeCharacter(char)
+                else
+                    client:notifyLocalized("invalidOrg")
+                end
+            end
+        end)
+
+        netstream.Hook("nutOrgDelete", function(client)
+            local char = client:getChar()
+            
+            if (char) then
+                local org = char:getOrganizationInfo()
+
+                if (org) then
+                    nut.org.delete(org:getID())
+                else
+                    client:notifyLocalized("invalidOrg")
+                end
+            end
+        end)
+
+        netstream.Hook("nutOrgKick", function(client, target)
+            
+        end)
+
+        netstream.Hook("nutOrgAssign", function(client, target, rank)
+            local char = client:getChar()
+            
+            if (char) then
+                local org = char:getOrganizationInfo()
+
+                if (org) then
+                    nut.org.delete(org:getID())
+                else
+                    client:notifyLocalized("invalidOrg")
+                end
+            end
+        end)
+
+        netstream.Hook("nutOrgChangeOwner", function(client, target)
+        
+        end)
+
+        netstream.Hook("nutOrgChangeValue", function(client, key, value)
+            local char = client:getChar()
+            
+            if (char) then
+                local org = char:getOrganizationInfo()
+
+                if (org) then
+                    local bool, reason = hook.Run("CanChangeOrganizationVariable", client, key, value)
+
+                    if (bool) then
+                        org:setData(key, value)
+                    else
+                        client:notifyLocalized(reason)
+                    end
+                else
+                    client:notifyLocalized("invalidOrg")
+                end
+            end
+        end)
+    end
 end
 
 --TODO: on player change the name, update the organization db!
