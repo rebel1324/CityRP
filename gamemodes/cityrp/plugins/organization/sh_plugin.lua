301f1c3c6efc2e6094cc8b08fbe2539e9e556a16
Kyu Yeon Lee <rebel1324@gmail.com>
2018-04-11 00:20:00 +0900

    Fixed another dumb error

--- gamemodes/cityrp/plugins/organization/sh_plugin.lua
+++ gamemodes/cityrp/plugins/organization/sh_plugin.lua
@@ -22,7 +22,7 @@ nut.util.include("sv_database.lua")
 
 if (CLIENT) then
     local myPanel
-    hook.Add("CreateMenuButtons", "nutEntities", function(tabs)
+    hook.Add("CreateMenuButtons", "nutOrganization", function(tabs)
         tabs["organization"] = function(panel)
             if (hook.Run("BuildEntitiesMenu", panel) != false) then
                 local org = LocalPlayer():getChar():getOrganizationInfo()
