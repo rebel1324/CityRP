ec28eae420df78b5fba2bcfbdb8bf93fc572fcf4
Kyu Yeon Lee <rebel1324@gmail.com>
2018-06-04 10:15:12 +0900

    Added Optimization, "E" Interactions

--- gamemodes/cityrp/schema/sh_schema.lua
+++ gamemodes/cityrp/schema/sh_schema.lua
@@ -30,13 +30,121 @@ nut.bent = nut.bent or {}
 nut.bent.list = {}
 
 if (CLIENT) then
-	nut.playerInteract.addFunc("giveMoney", {
-		nameLocalized = "giveMoney",
+	nut.playerInteract.addFunc("quickGiveMoney", {
+		nameLocalized = "quickGiveMoney",
 		callback = function(target)
+			Derma_StringRequest(L"setAmount", L"setAmountDesc", "", function(text)
+				if (text and text != "") then
+					client:ConCommand("say /give ".. tonumber(text) or "")
+				end
+			end, function() end)
+		end,
+		canSee = function(target)
+			local client = LocalPlayer()
+			if (!IsValid(target)) then return end
+			if (!target or !target:IsPlayer()) then return end
+			local char = target:getChar()
+			if (!char) then return end
+
+			return true
+		end
+	})
+
+	nut.playerInteract.addFunc("quickUseTie", {
+		nameLocalized = "quickUseTie",
+		callback = function(target)
+			local client = LocalPlayer()
+			local char = client:getChar()
+			local inv = char:getInv()
+			local item = inv:hasItem("tie")
+
+			netstream.Start("invAct", "Use", item.id, item.invID)
+		end,
+		canSee = function(target)
+			local client = LocalPlayer()
+			if (!IsValid(target)) then return end
+			if (!target:IsPlayer()) then return end
+			local char = client:getChar()
+			if (!char) then return end
+			local inv = char:getInv()
+			if (!inv) then return end
+			local hasItem = inv:hasItem("tie")
+			if (!hasItem) then return end
+			
+			return true
+		end
+	})
+
+	nut.playerInteract.addFunc("quickSearchPlayer", {
+		nameLocalized = "quickSearchPlayer",
+		callback = function(target)
+			client:ConCommand("say /search")
+		end,
+		canSee = function(target)
+			local client = LocalPlayer()
+			if (!IsValid(target)) then return end
+			local char = client:getChar()
+			if (!char) then return end
+			local class = char:getClass()
+			if (!class) then return end
+			local classData = nut.class.list[class]
+			if (!classData or !classData.law) then return end
+			if (!target:IsPlayer()) then return end
+
+			return true
+		end
+	})
+
+	nut.playerInteract.addFunc("quickGiveLicense", {
+		nameLocalized = "quickGiveLicense",
+		callback = function(target)
+			client:ConCommand("say /gunlicense")
+		end,
+		canSee = function(target)
+			local client = LocalPlayer()
+			if (!IsValid(target)) then return end
+			local char = client:getChar()
+			if (!char) then return end
+			local class = char:getClass()
+			if (!class) then return end
+			local classData = nut.class.list[class]
+			if (!classData or !classData.law) then return end
+			if (!target:IsPlayer()) then return end
+			if (target:getNetVar("license")) then return end
+			return true
+		end
+	})
+
+	nut.playerInteract.addFunc("quickRevokeLicense", {
+		nameLocalized = "quickRevokeLicense",
+		callback = function(target)
+			client:ConCommand("say /revokegunlicense")
+		end,
+		canSee = function(target)
+			local client = LocalPlayer()
+			if (!IsValid(target)) then return end
+			local char = client:getChar()
+			if (!char) then return end
+			local class = char:getClass()
+			if (!class) then return end
+			local classData = nut.class.list[class]
+			if (!classData or !classData.law) then return end
+			if (!target:IsPlayer()) then return end
+			if (!target:getNetVar("license")) then return end
+			return true
+		end
+	})
+
+	nut.playerInteract.addFunc("placeHit", {
+		nameLocalized = "placeHit",
+		callback = function(target)
+			local char = target:getChar()
 			print("hi")
 		end,
 		canSee = function(target)
-			return true
+			local char = target:getChar()
+
+			return char and char:getClass() == CLASS_HITMAN
 		end
 	})
 end
@@ -115,3 +223,42 @@ local noLoad = {
 function SCHEMA:PluginShouldLoad(uniqueID)
 	return noLoad[uniqueID] -- true = don't load the specified plugin.
 end
+
+if (SERVER) then
+	if timer.Exists("CheckHookTimes") then
+		timer.Remove("CheckHookTimes")
+	end
+end
+
+hook.Add("Initialize", "CITYRP_LAG_FIXER", function()
+	hook.Remove("PlayerTick", "TickWidgets")
+	hook.Remove( "PostGameSaved", "OnCreationsSaved" )
+	hook.Remove( "PopulateVehicles", "AddEntityContent" )
+	hook.Remove( "PopulatePostProcess", "AddPostProcess" )
+	hook.Remove( "PopulateNPCs", "AddNPCContent" )
+	hook.Remove( "DupeSaveAvailable", "UpdateDupeSpawnmenuAvailable")
+	hook.Remove( "DupeSaveUnavailable", "UpdateDupeSpawnmenuUnavailable")
+	hook.Remove( "DupeSaved", "DuplicationSavedSpawnMenu")
+	hook.Remove( "LoadGModSave", "LoadGModSave")
+	hook.Remove("RenderScreenspaceEffects", "RenderColorModify")
+	hook.Remove("RenderScreenspaceEffects", "RenderBloom")
+	hook.Remove("RenderScreenspaceEffects", "RenderToyTown")
+	hook.Remove("RenderScreenspaceEffects", "RenderTexturize")
+	hook.Remove("RenderScreenspaceEffects", "RenderSunbeams")
+	hook.Remove("RenderScreenspaceEffects", "RenderSobel")
+	hook.Remove("RenderScreenspaceEffects", "RenderSharpen")
+	hook.Remove("RenderScreenspaceEffects", "RenderMaterialOverlay")
+	hook.Remove("RenderScreenspaceEffects", "RenderMotionBlur")
+	hook.Remove("RenderScene", "RenderStereoscopy")
+	hook.Remove("RenderScene", "RenderSuperDoF")
+	hook.Remove("GUIMousePressed", "SuperDOFMouseDown")
+	hook.Remove("GUIMouseReleased", "SuperDOFMouseUp")
+	hook.Remove("PreventScreenClicks", "SuperDOFPreventClicks")
+	hook.Remove("PostRender", "RenderFrameBlend")
+	hook.Remove("PreRender", "PreRenderFrameBlend")
+	hook.Remove("Think", "DOFThink")
+	hook.Remove("RenderScreenspaceEffects", "RenderBokeh")
+	hook.Remove("NeedsDepthPass", "NeedsDepthPass_Bokeh")
+	hook.Remove("PostDrawEffects", "RenderWidgets")
+	hook.Remove("PostDrawEffects", "RenderHalos")
+end)
