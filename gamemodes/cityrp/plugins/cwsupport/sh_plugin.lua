f2eb801b54019f86f3ce7e798af9028c7d275a54
Kyu Yeon Lee <rebel1324@gmail.com>
2018-06-04 10:17:57 +0900

    Fixed Weapon Reliability.

--- gamemodes/cityrp/plugins/cwsupport/sh_plugin.lua
+++ gamemodes/cityrp/plugins/cwsupport/sh_plugin.lua
@@ -73,7 +73,7 @@ function PLUGIN:InitializedPlugins()
 			ITEM.exRender = true
 
 			function ITEM:getDesc()
-				return L("ammoDesc", self:getQuantity(), L(self.ammo))
+				return L("ammoDesc", self.getQuantity and self:getQuantity() or "", L(self.ammo))
 			end
 		end
 	end
@@ -378,6 +378,90 @@ function PLUGIN:InitializedPlugins()
 						end
 					end
 					
+
+
+					-- TODO: Remove THOTS
+					-- jesus fuck this is one messy shit
+					-- On player eqipped the item, Gives a weapon to player and load the ammo data from the item.
+					ITEM.functions.Equip = {
+						name = "Equip",
+						tip = "equipTip",
+						icon = "icon16/tick.png",
+						onRun = function(item)
+							local client = item.player
+							local items = client:getChar():getInv():getItems()
+
+							client.carryWeapons = client.carryWeapons or {}
+
+							for k, v in pairs(items) do
+								if (v.id != item.id) then
+									local itemTable = nut.item.instances[v.id]
+									
+									if (!itemTable) then
+										client:notifyLocalized("tellAdmin", "wid!xt")
+
+										return false
+									else
+										if (itemTable.isWeapon and client.carryWeapons[item.weaponCategory] and itemTable:getData("equip")) then
+											client:notifyLocalized("weaponSlotFilled")
+
+											return false
+										end
+									end
+								end
+							end
+							
+							if (client:HasWeapon(item.class)) then
+								client:StripWeapon(item.class)
+							end
+
+							local weapon = client:Give(item.class)
+
+							if (IsValid(weapon)) then
+								timer.Simple(0, function()
+									client:SelectWeapon(weapon:GetClass())
+									--client:SetActiveWeapon(weapon)
+								end)
+								client.carryWeapons[item.weaponCategory] = weapon
+								client:EmitSound("items/ammo_pickup.wav", 80)
+
+								-- Remove default given ammo.
+								if (client:GetAmmoCount(weapon:GetPrimaryAmmoType()) == weapon:Clip1() and item:getData("ammo", 0) == 0) then
+									client:RemoveAmmo(weapon:Clip1(), weapon:GetPrimaryAmmoType())
+								end
+								item:setData("equip", true)
+
+								weapon:SetClip1(item:getData("ammo", 0))
+
+								if (item.onEquipWeapon) then
+									item:onEquipWeapon(client, weapon)
+								end
+							else
+								print(Format("[Nutscript] Weapon %s does not exist!", item.class))
+							end
+
+							return false
+						end,
+						onCanRun = function(item)
+							return (!IsValid(item.entity) and item:getData("equip") != true)
+						end
+					}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 					ITEM.functions.use = {
 					name = "Detach",
 					tip = "useTip",
