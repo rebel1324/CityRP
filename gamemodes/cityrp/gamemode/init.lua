1d8c1d13a207fb7e3461b2395a708ff38e4f46cc
Kyu Yeon Lee <rebel1324@gmail.com>
2018-06-04 10:18:21 +0900

    Removed Minge Acts.

--- gamemodes/cityrp/gamemode/init.lua
+++ gamemodes/cityrp/gamemode/init.lua
@@ -1,2 +1,11 @@
 AddCSLuaFile("cl_init.lua")
 DeriveGamemode("nutscript")
+
+concommand.Remove("gm_save")
+concommand.Remove("dupe_arm")
+
+concommand.Add("gm_save", function(client, command, arguments)
+end)
+
+concommand.Add("dupe_arm", function(client, command, arguments)
+end)
