e333a812e8c94e51f8eab4dd2138943d4e7bfc10
Kyu Yeon Lee <rebel1324@gmail.com>
2018-07-10 08:12:41 +0900

    still working on.

--- gamemodes/cityrp/plugins/crafting/cl_vgui.lua
+++ gamemodes/cityrp/plugins/crafting/cl_vgui.lua
@@ -1,3 +1,30 @@
 local PANEL = {}
 
+function PANEL:Init()
+    self:Dock(TOP)
+end
+
+function PANEL:setBlueprint()
+
+end
+
+vgui.Register("nutCraftingRow", PANEL, "DFrame")
+
+local PANEL = {}
+
+function PANEL:Init()
+    if (IsValid(nut.gui.crafting)) then
+        nut.gui.crafting:Remove()
+    end
+    nut.gui.crafting = self
+
+    self:SetSize(800, 600)
+    self:Center()
+    self:MakePopup()
+
+    for id, info in pairs(nut.craft.list) do
+        
+    end
+end
+
 vgui.Register("nutCraftingMenu", PANEL, "DFrame")
--- gamemodes/cityrp/plugins/crafting/sh_ingredients.lua
+++ gamemodes/cityrp/plugins/crafting/sh_ingredients.lua
@@ -1,1 +1,31 @@
---a
+--[[
+    I'm not going to make shit load of item files just for this shit
+]]
+
+local generateItems = {
+    mineral = {
+
+    },
+    chem = {
+
+    },
+    elec = {
+
+    }
+}
+
+function PLUGIN:InitializedPlugins()
+    for prefix, items in pairs(generateItems) do
+        for uniqueID, itemInfo in pairs(items) do
+            local uniqueID = prefix .. "_" .. uniqueID
+
+            local ITEM = nut.item.register(uniqueID, nil, nil, nil, true)
+            ITEM.name = itemInfo.name
+            ITEM.price = itemInfo.price * itemInfo.maxQuantity
+            ITEM.model = itemInfo.model
+			ITEM.onGetDropModel = function() return itemInfo.dropModel end
+            ITEM.isStackable = true
+            ITEM.maxQuantity = ammoInfo.maxQuantity
+        end
+	end
+end
