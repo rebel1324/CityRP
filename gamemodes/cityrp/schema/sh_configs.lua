03de06c72fb6f2ccd1c9225068a13f11ed87cbea
Kyu Yeon Lee <rebel1324@gmail.com>
2018-07-22 20:51:09 +0900

    Made sh_config more international.

--- gamemodes/cityrp/schema/sh_configs.lua
+++ gamemodes/cityrp/schema/sh_configs.lua
@@ -320,6 +320,7 @@ nut.config.add("doorTax", 100, "door tax.", nil, {
 	category = "schema"
 })
 
+
 -- yeah yeah i'm going to make this short, okay?
 local function pianoOnly(client)
 	local char = client:getChar()
@@ -481,6 +482,15 @@ function nut.bent.add(entClass, entModel, entName, entMax, entPrice, buyConditio
 	return nut.bent.list[entClass]
 end
 
+function nut.bent.jobInclude()
+end
+
+function nut.bent.jobExclude()
+end
+
+function nut.bent.jobTeam()
+end
+
 nut.bent.add("printer_tier1", "models/rebel1324/mprint.mdl", "printerNameTier1", 1, 20000, notLaw)
 nut.bent.add("printer_tier2", "models/rebel1324/mprint.mdl", "printerNameTier2", 1, 30000, notLaw)
 nut.bent.add("printer_tier3", "models/rebel1324/mprint.mdl", "printerNameTier3", 1, 50000, notLaw)
@@ -488,157 +498,60 @@ nut.bent.add("printer_tier4", "models/rebel1324/mprint.mdl", "printerNameTier4",
 nut.bent.add("printer_tier5", "models/rebel1324/mprint.mdl", "printerNameTier5", 1, 85000, notLaw)
 nut.bent.add("printer_tier6", "models/rebel1324/mprint.mdl", "printerNameTier6", 1, 100000, notLaw)
 nut.bent.add("printer_tier7", "models/rebel1324/mprint.mdl", "printerNameTier7", 1, 130000, notLaw)
-
 nut.bent.add("nut_vnd_food", "models/props_wasteland/kitchen_stove002a.mdl", "foodVendor", 1, 800, cookOnly)
 nut.bent.add("nut_vnd_soda", "models/rebel1324/sodavendor.mdl", "sodaVendor", 1, 1000, cookOnly)
 nut.bent.add("nut_vnd_covfefe", "models/props/commercial/coffeemachine01.mdl", "coffeeVendor", 1, 4500, cookOnly)
-
 nut.bent.add("nut_microwave", "models/props/cs_office/microwave.mdl", "microwave", 2, 400, cookOnly)
 nut.bent.add("nut_stove", "models/props_c17/furnitureStove001a.mdl", "stove", 1, 1500, cookOnly)
-
--- weed is removed temporaly
---nut.bent.add("nut_d_lamp", "models/gonzo/weedb/lamp2.mdl", "weedLampName", 4, 800, mobOnly)
---nut.bent.add("nut_d_pot", "models/gonzo/weedb/pot2.mdl", "weedPotName", 20, 1000, mobOnly)
-
 nut.bent.add("nut_hobocan", "models/props_junk/MetalBucket02a.mdl", "hoboCanName", 2, 3000, hoboOnly)
-
 nut.bent.add("nut_attrib_gun", "models/props_c17/doll01.mdl", "gunBoosterName", 2, 3000, dealerOnly)
-
 nut.bent.add("nut_vnd_medical", "models/rebel1324/medicvendor.mdl", "medicalVendorName", 1, 1000, docOnly)
-
 nut.bent.add("synthesizer", "models/tnf/synths.mdl", "synthesizer", 1, 3000, pianoOnly)
 nut.bent.add("synthesizer_guitar", "models/tnf/synth.mdl", "synthesizerGuitar", 1, 3000, pianoOnly)
 nut.bent.add("synthesizer_piano", "models/tnf/synth.mdl", "synthesizerPiano", 1, 3000, pianoOnly)
 nut.bent.add("synthesizer_violin", "models/tnf/synth.mdl", "synthesizerViolin", 1, 3000, pianoOnly)
-
-nut.bent.add("wdj_mastercontroller", "models/props_lab/citizenradio.mdl", "deejayController", 1, 10000, djOnly)
-nut.bent.add("wdj_speaker", "models/props_lab/citizenradio.mdl", "deejaySpeaker", 1, 10000, djOnly)
-
-nut.bent.add("wdj_radio", "models/props_lab/citizenradio.mdl", "deejayRadio", 1, 5000, all)
 nut.bent.add("nut_seller", "models/rebel1324/nmrih_cash_register.mdl", "checkoutName", 1, 2500, all)
-
 nut.bent.add("nut_craftingtable", "models/props_wasteland/controlroom_desk001b.mdl", "craftingTable", 1, 10000, all)
---nut.bent.add("nut_loadingtable", "models/props_wasteland/controlroom_desk001b.mdl", "loadingTable", 1, 500, all)
-
 nut.bent.add("sent_bingle_simplenoti", "models/props/cs_assault/chaintrainstationsign.mdl", "signSmall", 2, 400, all)
 nut.bent.add("sent_bingle_simplenoti_orlong", "models/squad/sf_plates/sf_plate2x5.mdl", "signMedium", 2, 400, all)
 nut.bent.add("sent_bingle_simplenoti_mdbig", "models/hunter/plates/plate1x3.mdl", "singBig", 2, 400, all)
 nut.bent.add("nut_attrib_punch", "models/props_lab/huladoll.mdl", "strBoosterName", 2, 1000, all)
-
 nut.bent.add("rprotect_terminal", "models/props_phx/rt_screen.mdl", "survTerminal", 1, 2000, all)
 nut.bent.add("rprotect_scanner", "models/Items/battery.mdl", "survScanner", 4, 1500, all)
 nut.bent.add("rprotect_camera", "models/tools/camera/camera.mdl", "survCamera", 1, 2000, all)
-
 nut.bent.add("nut_mapbeacon", "models/props_combine/combine_mine01.mdl", "mapBeacon", 1, 2000, all)
-
 nut.bent.add("frame_flag", "models/rebel1324/z018.mdl", "frameFlag", 1, 6500, all)
 nut.bent.add("frame_rect_big", "models/rebel1324/painting_hexed_rect_big.mdl", "frameRectBig", 1, 4000, all)
 nut.bent.add("frame_rect", "models/rebel1324/painting_hexed_rect.mdl", "frameRect", 1, 2000, all)
 nut.bent.add("frame_hort", "models/rebel1324/painting_hexed.mdl", "frameHort", 1, 2000, all)
 
--- BITCOIN MINERS
---[[
-	nut.bent.add("bm2_bitminer_1", "models/bitminers2/bitminer_1.mdl", "bitminer", 1, 5000, busiOnly)
-	nut.bent.add("bm2_bitminer_2", "models/bitminers2/bitminer_2.mdl", "bitminer2", 1, 8000, busiOnly)
-	nut.bent.add("bm2_bitminer_rack", "models/bitminers2/bitminer_rack.mdl", "bitminerRack", 1, 5000, busiOnly)
-	nut.bent.add("bm2_bitminer_server", "models/bitminers2/bitminer_2.mdl", "bitminerServer", 8, 25000, busiOnly)
-	nut.bent.add("bm2_extention_lead", "models/bitminers2/bitminer_plug_3.mdl", "bitminerExtend", 1, 3000, busiOnly)
-	nut.bent.add("bm2_fuel", "models/props_junk/gascan001a.mdl", "bitminerFuel", 1, 2500, busiOnly)
-	nut.bent.add("bm2_generator", "models/bitminers2/generator.mdl", "bitminerGenerator", 1, 4000, busiOnly)
-	nut.bent.add("bm2_power_lead", "models/bitminers2/bitminer_plug_2.mdl", "bitminerPlug", 3, 3500, busiOnly)
-]]
-
---[[
--- need to get better shit.
-local bentstr = nut.bent.add("nut_storage", "models/rebel1324/footlocker.mdl", "5x3 아이템 상자", 2, 1000, all)
-hook.Add("InitializedPlugins", "registerFootlockers", function()
-	STORAGE_DEFINITIONS["models/rebel1324/footlocker.mdl"] = {
-		name = "Crate",
-		desc = "5x3의 공간을 가지고 있는 상자입니다.",
-		width = 5,
-		height = 3,
-		onOpen = function(entity, activator)
-			timer.Simple(1, function()
-				local seq = entity:LookupSequence("open")
-				entity:ResetSequence(seq)
-			end)
-
-			timer.Simple(3, function()
-				if (entity and IsValid(entity)) then
-					local seq = entity:LookupSequence("closed")
-					entity:ResetSequence(seq)
-				end
-			end)
-		end,
-	}
-end)
-
-bentstr.onSpawn = function(entity, client, char, info)
-	local data = STORAGE_DEFINITIONS[info.model:lower()]
-
-	if (data) then
-		entity:SetModel(info.model)
-		entity:SetSolid(SOLID_VPHYSICS)
-		entity:PhysicsInit(SOLID_VPHYSICS)
-
-		local ca, cb = entity:GetCollisionBounds()
-		entity:SetPos(entity:GetPos() + cb)
-
-		nut.item.newInv(0, "st"..data.name, function(inventory)
-			inventory.vars.isStorage = true
-
-			if (IsValid(entity)) then
-				entity:setInventory(inventory)
-			end
-		end)
-
-		local phys = entity:GetPhysicsObject()
-
-		if (phys) then
-			phys:Wake()
-		end
-	else
-		print("server got request that contains wrong storage model STORAGE_DEFINITIONS.")
-		entity:Remove()
-	end
-end
---]]
-
--- Translation by Omar Saleh Assadi / AngryBaldMan
---[[
-	nut.tips = {
-		--'대부분의 아이템은 Ctrl을 누르고 클릭하면 바로 사용할 수 있습니다.',
-		'Remember, your salary is automatically deposited into your bank account. Make sure to bring cash!',
-		'Did you know? The inventory can also be opened directly by pressing \'F3\'.',
-		'Reptitive actions will help train your skills.',
-		'Did you know? Punching-bag dolls will train your strength skill when hit.',
-		'Did you know? Reading books are great for boosting your stats!',
-		'If you\'d like, you can hide these tips via the quick settings. These can be found by holding \'C\' and clicking the gear in the upper right hand corner.',
-		'If you\'d like, you can hide these tips via the quick settings. These can be found by holding \'C\' and clicking the gear in the upper right hand corner.',
-		'If you\'d like, you can hide these tips via the quick settings. These can be found by holding \'C\' and clicking the gear in the upper right hand corner.',
-		'Did you know? Shooting target dolls will train your firearms skill.',
-		'Certain traders will only sell things to you if you have the right job.',
-		'Did you know? Mobsters, gangsters, and police can communicate with eachother directly using the \'/team\' command.',
-		'Bug reports are resolved much quicker if you attach an image or video.',
-		'Bug reports are resolved much quicker if you attach an image or video.',
-		'Bug reports are resolved much quicker if you attach an image or video.',
-		'Did you know? You can earn ssome extra money using money printers.',
-		'Police receive rewards for confiscating money printers.',
-		'Please separate in-character chat and out of character chat!',
-		'Please separate in-character chat and out of character chat!',
-		'Please check MOTD and IC / OOC before asking for help.',
-		'Please check MOTD and IC / OOC before asking for help.',
-		'Please check MOTD and IC / OOC before asking for help.',
-		'Having an issue? You can file a report with an administrator using the report command.',
-		'Having an issue? You can file a report with an administrator using the report command.',
-		'Having an issue? You can file a report with an administrator using the report command.',
-		'Having an issue? You can file a report with an administrator using the report command.',
-	}
-]]--
-
 nut.tips = {
-	"지속적으로 서버 정보를 받고싶다면 가입하세요:  https://steamcommunity.com/groups/lolsdarkrpserver",
-	"어드민이 없을때에는 커뮤니티에 남겨주세요: https://steamcommunity.com/groups/lolsdarkrpserver",
-	"어드민이 있을때에는 !신고 또는 !report로 신고가 가능합니다.",
-	"어드민 호출은 @ [할말] 로 호출이 가능합니다.",
+	--'대부분의 아이템은 Ctrl을 누르고 클릭하면 바로 사용할 수 있습니다.',
+	'Remember, your salary is automatically deposited into your bank account. Make sure to bring cash!',
+	'Did you know? The inventory can also be opened directly by pressing \'F3\'.',
+	'Reptitive actions will help train your skills.',
+	'Did you know? Punching-bag dolls will train your strength skill when hit.',
+	'Did you know? Reading books are great for boosting your stats!',
+	'If you\'d like, you can hide these tips via the quick settings. These can be found by holding \'C\' and clicking the gear in the upper right hand corner.',
+	'If you\'d like, you can hide these tips via the quick settings. These can be found by holding \'C\' and clicking the gear in the upper right hand corner.',
+	'If you\'d like, you can hide these tips via the quick settings. These can be found by holding \'C\' and clicking the gear in the upper right hand corner.',
+	'Did you know? Shooting target dolls will train your firearms skill.',
+	'Certain traders will only sell things to you if you have the right job.',
+	'Did you know? Mobsters, gangsters, and police can communicate with eachother directly using the \'/team\' command.',
+	'Bug reports are resolved much quicker if you attach an image or video.',
+	'Bug reports are resolved much quicker if you attach an image or video.',
+	'Bug reports are resolved much quicker if you attach an image or video.',
+	'Did you know? You can earn ssome extra money using money printers.',
+	'Police receive rewards for confiscating money printers.',
+	'Please separate in-character chat and out of character chat!',
+	'Please separate in-character chat and out of character chat!',
+	'Please check MOTD and IC / OOC before asking for help.',
+	'Please check MOTD and IC / OOC before asking for help.',
+	'Please check MOTD and IC / OOC before asking for help.',
+	'Having an issue? You can file a report with an administrator using the report command.',
+	'Having an issue? You can file a report with an administrator using the report command.',
+	'Having an issue? You can file a report with an administrator using the report command.',
+	'Having an issue? You can file a report with an administrator using the report command.',
 }
+
+hook.Run("OnRegisterPurchaseList")
