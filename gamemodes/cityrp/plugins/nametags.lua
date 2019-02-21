20fca34f883d318893d79004478dfe622e3e0cbb
Kyu Yeon Lee <rebel1324@gmail.com>
2019-02-21 12:58:00 +0900

    Super Optimization on nametags. (and behavior changes.)

--- gamemodes/cityrp/plugins/nametags.lua
+++ gamemodes/cityrp/plugins/nametags.lua
@@ -5,9 +5,17 @@ PLUGIN.desc = "This plugin displays 3D2D Player information on the top of the he
 
 if (SERVER) then return end
 
+local lerp = Lerp
+local surface = surface
+local frameTime = FrameTime
+local colorAlpha = ColorAlpha
+local cam = cam
+local util = util
+local draw = draw
+
 local ntMax = 512
 local ntPos, ntView, ntAng, ntEye, ntScale = Vector(), Vector(), Angle(), Angle(), 0.05
-local ntCol, ntX, ntY, ntDwt, ntAlpha
+local ntCol, ntX, ntY, ntDwt
 local ntShadow = Color(0, 0, 0)
 local ntWhite = Color(255, 255, 255)
 local ntAFK = Color(102, 102, 204)
@@ -31,13 +39,13 @@ btNameTag.info = {
 			local info = char:getOrganizationInfo()
 			local colData = info:getData("nameCol", ntOrg)
 
-			return btNameTag:drawText(info:getName(), ntX, ntY, ColorAlpha(colData, ntCol.a), 1)
+			return btNameTag:drawText(info:getName(), ntX, ntY, colorAlpha(colData, ntCol.a), 1)
 		end,
 	},
 	{
 		canDraw = function(client, char) return client:isLegBroken() end,
 		doDraw = function(client, ntX, ntY, ntCol)
-			return btNameTag:drawText(L"legBroken", ntX, ntY, ColorAlpha(ntWanted, ntCol.a), 1)
+			return btNameTag:drawText(L"legBroken", ntX, ntY, colorAlpha(ntWanted, ntCol.a), 1)
 		end,
 	},
 	{
@@ -49,74 +57,80 @@ btNameTag.info = {
 					text = L(b[2])
 				end
 			end
-			return btNameTag:drawText(text, ntX, ntY, ColorAlpha(ntWanted, ntCol.a), 1)
+			return btNameTag:drawText(text, ntX, ntY, colorAlpha(ntWanted, ntCol.a), 1)
 		end,
 	},
 	{
 		canDraw = function(client, char, ntRagdoll) return client:Alive() and IsValid(ntRagdoll) end,
 		doDraw = function(client, ntX, ntY, ntCol)
-			return btNameTag:drawText(L"stunned", ntX, ntY, ColorAlpha(ntAFK, ntCol.a), 1)
+			return btNameTag:drawText(L"stunned", ntX, ntY, colorAlpha(ntAFK, ntCol.a), 1)
 		end,
 	},
 	{
 		canDraw = function(client, char) return !client:Alive() end,
 		doDraw = function(client, ntX, ntY, ntCol)
-			return btNameTag:drawText(L"dead", ntX, ntY, ColorAlpha(ntWanted, ntCol.a), 1)
+			return btNameTag:drawText(L"dead", ntX, ntY, colorAlpha(ntWanted, ntCol.a), 1)
 		end,
 	},
 	{
 		canDraw = function(client) return client:IsAFK() end,
 		doDraw = function(client, ntX, ntY, ntCol)
 			local s = btNameTag.afkPhrases[math.floor((CurTime()/4 + client:EntIndex())%#btNameTag.afkPhrases) + 1]
-			return btNameTag:drawText(s, ntX, ntY, ColorAlpha(ntAFK, ntCol.a), 1)
+			return btNameTag:drawText(s, ntX, ntY, colorAlpha(ntAFK, ntCol.a), 1)
 		end,
 	},
 	{
 		canDraw = function(client) return client:getNetVar("onHit") end,
 		doDraw = function(client, ntX, ntY, ntCol)
-			return btNameTag:drawText(L"onHit", ntX, ntY, ColorAlpha(ntWanted, ntCol.a), 1)
+			return btNameTag:drawText(L"onHit", ntX, ntY, colorAlpha(ntWanted, ntCol.a), 1)
 		end,
 	},
 	{
 		canDraw = function(client) return false end,
 		doDraw = function(client, ntX, ntY, ntCol)
-			return btNameTag:drawText(L"afk", ntX, ntY, ColorAlpha(ntAFK, ntCol.a), 1)
+			return btNameTag:drawText(L"afk", ntX, ntY, colorAlpha(ntAFK, ntCol.a), 1)
 		end,
 	},
 	{
 		canDraw = function(client) return client:getNetVar("restricted") end,
 		doDraw = function(client, ntX, ntY, ntCol)
-			return btNameTag:drawText(L"tied", ntX, ntY, ColorAlpha(ntWanted, ntCol.a), 1)
+			return btNameTag:drawText(L"tied", ntX, ntY, colorAlpha(ntWanted, ntCol.a), 1)
 		end,
 	},
 	{
 		canDraw = function(client) return client:getNetVar("hitman") == ntLocalPlayer end,
 		doDraw = function(client, ntX, ntY, ntCol)
-			return btNameTag:drawText(L"hitTarget", ntX, ntY, ColorAlpha(ntWanted, ntCol.a), 1)
+			return btNameTag:drawText(L"hitTarget", ntX, ntY, colorAlpha(ntWanted, ntCol.a), 1)
 		end,
 	},
 	{
 		canDraw = function(client) return client:getNetVar("license", false) end,
 		doDraw = function(client, ntX, ntY, ntCol)
-			return btNameTag:drawText(L"hasLicense", ntX, ntY, ColorAlpha(ntGood, ntCol.a), 1)
+			return btNameTag:drawText(L"hasLicense", ntX, ntY, colorAlpha(ntGood, ntCol.a), 1)
+		end,
+	},
+	{
+		canDraw = function(client) return client.isBanned and client:isBanned() end,
+		doDraw = function(client, ntX, ntY, ntCol)
+			return btNameTag:drawText(L"hasBanni", ntX, ntY, colorAlpha(ntWanted, ntCol.a), 1)
 		end,
 	},
 	{
 		canDraw = function(client) return client:getNetVar("searchWarrant", false) end,
 		doDraw = function(client, ntX, ntY, ntCol)
-			return btNameTag:drawText(L"onWarrant", ntX, ntY, ColorAlpha(ntGood, ntCol.a), 1)
+			return btNameTag:drawText(L"onWarrant", ntX, ntY, colorAlpha(ntGood, ntCol.a), 1)
 		end,
 	},
 	{
 		canDraw = function(client, char) return char:getData("wanted") end,
 		doDraw = function(client, ntX, ntY, ntCol)
-			return btNameTag:drawText(L"onWanted", ntX, ntY, ColorAlpha(ntWanted, ntCol.a), 1)
+			return btNameTag:drawText(L"onWanted", ntX, ntY, colorAlpha(ntWanted, ntCol.a), 1)
 		end,
 	},
 	{
 		canDraw = function(client) return client:isArrested() end,
 		doDraw = function(client, ntX, ntY, ntCol)
-			return btNameTag:drawText(L"arrested", ntX, ntY, ColorAlpha(ntWanted, ntCol.a), 1)
+			return btNameTag:drawText(L"arrested", ntX, ntY, colorAlpha(ntWanted, ntCol.a), 1)
 		end,
 	},
 }
@@ -154,6 +168,7 @@ hook.Add("LoadFonts", "nutFontNametag", function(font, genericFont)
 	end
 end)
 
+local offset = Vector(0, 0, 16)
 function btNameTag:getHead(entity)
 	local pos
 	local bone = entity:GetAttachment(entity:LookupAttachment("eyes"))
@@ -162,102 +177,143 @@ function btNameTag:getHead(entity)
 	if not pos then
 		local bone = entity:LookupBone("ValveBiped.Bip01_Head1")
 		
-		pos = bone and entity:GetBonePosition(bone) or entity:EyePos()
+		pos = bone and entity:GetBonePosition(bone) + offset or entity:EyePos() + offset
 	end
 	
-	return pos
+	return pos + offset
 end
 
-function btNameTag:drawText(text, x, y, tCol, a)
-	draw.SimpleText(text, btNameTag.font[2 + 2*(a or 0)], x, y, ColorAlpha(ntShadow, math.min(tCol.a * 2, 255)), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-	draw.SimpleText(text, btNameTag.font[1 + 2*(a or 0)], x - 1, y - 1, tCol, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-end
 
 local ntChar, ntClass, ntClassInfo, ntRagdoll
 
 local lastCall = FrameNumber()
 local lastCallPlayer
 local originMatrix = Matrix()
-hook.Add("PostPlayerDraw", "btNameTag", function(client)
-	if (client:GetNoDraw() != true) then
-		ntView = EyePos()
+local hullSize = 3
+local maxVector = Vector(1, 1, 1) * hullSize
+local minVector = maxVector * -hullSize
+local detectRange = 512
+local targetEntity
+timer.Create('btNameTagFinder', .3, 0, function()
+    local localPlayer = LocalPlayer()
+	if (IsValid(localPlayer)) then
+		local data = {}
+			data.start = localPlayer:GetShootPos()
+			data.endpos = data.start + localPlayer:GetAimVector()*detectRange
+			data.filter = localPlayer
+			data.mins = minVector
+			data.maxs = maxVector
+			data.mask = MASK_SHOT
+		local trace = util.TraceHull(data)
+
+		targetEntity = trace.Entity
+
+		if (IsValid(targetEntity) and not targetEntity:IsPlayer()) then
+			local data = {}
+				data.start = localPlayer:GetShootPos()
+				data.endpos = data.start + localPlayer:GetAimVector()*detectRange
+				data.filter = localPlayer
+				data.mask = MASK_SHOT
+			local trace = util.TraceLine(data)
+			
+			if (trace.Entity:IsPlayer()) then
+				targetEntity = trace.Entity
+			end
+		end
+    end
+end)
+
+local popFraction = 0
+local popSpeed = .3
+local lastEntity
+
+function btNameTag:drawText(text, x, y, tCol, a)
+	draw.SimpleText(text, btNameTag.font[2 + 2*(a or 0)], x, y, colorAlpha(ntShadow, math.min(tCol.a * 2, 255)), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+	draw.SimpleText(text, btNameTag.font[1 + 2*(a or 0)], x - 1, y - 1, tCol, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+end
+
+local function lookAtMeAngle(entityA, entityB)
+    local ang = entityB:GetPos() - entityA:GetPos()
+    ang:Normalize()
+    ang = ang:Angle()
+    ang:RotateAroundAxis(ang:Up(), -90)
+    ang:RotateAroundAxis(ang:Forward(), 90)
+
+    return ang
+end
+
+hook.Add("PostDrawTranslucentRenderables", "btNameTag", function(client)
+    local localPlayer = LocalPlayer()
+	local client = targetEntity
+
+	if (IsValid(client) and client:GetNoDraw() != true and client:IsPlayer()) then
+        if (lastEntity != client) then
+            popFraction = 0
+            lastEntity = client
+        end
+
+        popFraction = lerp(frameTime() * popSpeed, popFraction, 1)
+        local viewScale = nut.ease.easeOutElastic(popFraction, 1, 0, 1) * .1
+
 		ntPos = btNameTag:getHead(client)
-		ntDist = math.Clamp(ntView:Distance(ntPos) / ntMax, 0, 1)
-		if (ntDist >= 1) then return end
-	
 		ntChar = client:getChar()
 		if (ntChar or client:IsBot()) then
-			client.pixVis = client.pixVis or util.GetPixelVisibleHandle()
-			local visibility = util.PixelVisible(ntPos, 8, client.pixVis)
-			if (visibility < 0) then return end
+			cam.Start3D2D(ntPos, lookAtMeAngle(localPlayer, client), viewScale)
+				xpcall(function()
+				    local ntX, ntY = 0, 0
+					ntCol = color_white
+					ntClass = ntChar:getClass()
 
-			cam.Start2D()
-				local scr = ntPos:ToScreen()
-				local scale = math.max(math.abs(1 - ntDist) * .8, 0.00001)
-				scale = scale * .8 
-				local ntX, ntY = 0, 0
-
-				if (not IsValid(client.mx)) then
-					client.mx = Matrix()
-				end
-				client.mx:SetTranslation(Vector(scr.x, scr.y - 16 - scale * 100, 1))
-				client.mx:SetScale(Vector(scale, scale, 1))
-
-
-				cam.PushModelMatrix(client.mx)
-					xpcall(function()
-						ntCol = Color(255, 255, 255)
-						ntAlpha = 255*math.min(visibility, (1 - ntDist))
-						ntClass = ntChar:getClass()
-
-						local description = ntChar:getDesc()
-						if (description and description != "") then
-							if (!client.oldDesc or client.oldDesc != description) then
-								local preCalcFont = btNameTag.font[2 + 2*(1 or 0)]
-								client.oldDescObject = nut.util.wrapText(description, 900, preCalcFont)
-								client.oldDesc = description
-							end
-							
-							for i = 1, #client.oldDescObject do
-								btNameTag:drawText(client.oldDescObject[#client.oldDescObject - i + 1], ntX, ntY, ColorAlpha(ntGreen, ntAlpha), 1)
-								ntY = ntY - 60
-							end
+					local description = ntChar:getDesc()
+					if (description and description != "") then
+						if (!client.oldDesc or client.oldDesc != description) then
+							local preCalcFont = btNameTag.font[2 + 2*(1 or 0)]
+							client.oldDescObject = nut.util.wrapText(description, 900, preCalcFont)
+							client.oldDesc = description
 						end
 						
-						local name = hook.Run("ShouldAllowScoreboardOverride", client, "name") and hook.Run("GetDisplayedName", client) or client:Nick()
-						if (ntClass or client:IsBot()) then
-							if (client:IsBot()) then
-								btNameTag:drawText(name, ntX, ntY, ColorAlpha(nut.config.get("color"), ntAlpha))
-								ntY = ntY - 80
-							else
-								ntClassInfo = nut.class.list[ntClass]
-								if (ntClassInfo) then
-									btNameTag:drawText(L(ntClassInfo.name), ntX, ntY, ColorAlpha(ntCol, ntAlpha), 1)
-									ntY = ntY - 75
-								end
-			
-								btNameTag:drawText(name, ntX, ntY, ColorAlpha(ntClassInfo.color or nut.config.get("color"), ntAlpha))
-								ntY = ntY - 80
-							end
+						for i = 1, #client.oldDescObject do
+							btNameTag:drawText(client.oldDescObject[#client.oldDescObject - i + 1], ntX, ntY, colorAlpha(ntGreen, 255), 1)
+							ntY = ntY - 60
+						end
+					end
+					
+					local name = hook.Run("ShouldAllowScoreboardOverride", client, "name") and hook.Run("GetDisplayedName", client) or client:Nick()
+					if (ntClass or client:IsBot()) then
+						if (client:IsBot()) then
+							btNameTag:drawText(name, ntX, ntY, colorAlpha(nut.config.get("color"), 255))
+							ntY = ntY - 80
 						else
-							ntY = ntY - 25
-							btNameTag:drawText(name, ntX, ntY, ColorAlpha(nut.config.get("color"), ntAlpha))
-						end
-						
-						for _, info in ipairs(btNameTag.info) do
-							if (info.canDraw(client, ntChar, ntRagdoll)) then
-								info.doDraw(client, ntX, ntY, ColorAlpha(ntCol, ntAlpha))
-								ntY = ntY -  60
+							ntClassInfo = nut.class.list[ntClass]
+							if (ntClassInfo) then
+								btNameTag:drawText(L(ntClassInfo.name), ntX, ntY, colorAlpha(ntCol, 255), 1)
+								ntY = ntY - 75
 							end
+			
+							btNameTag:drawText(name, ntX, ntY, colorAlpha(ntClassInfo.color or nut.config.get("color"), 255))
+							ntY = ntY - 80
 						end
-					end, function(...) print(...) end)
-				cam.PopModelMatrix()
-			cam.End2D()
+					else
+						ntY = ntY - 25
+						btNameTag:drawText(name, ntX, ntY, colorAlpha(nut.config.get("color"), 255))
+					end
+					
+					for _, info in ipairs(btNameTag.info) do
+						if (info.canDraw(client, ntChar, ntRagdoll)) then
+							info.doDraw(client, ntX, ntY, colorAlpha(ntCol, 255))
+							ntY = ntY -  60
+						end
+					end
+				end, function(...) print(...) end)
+			cam.End3D2D()
 		end
 		
 		ntChar = nil
 		ntClass = nil
 		ntClassInfo = nil
 		ntRagdoll = nil		
+        lastEntity = client
+    else
+        popFraction = 0
 	end
 end)
