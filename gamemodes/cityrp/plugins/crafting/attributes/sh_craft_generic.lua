d1d2bdc4702d0babe615e48bb60fb560b83f2100
Kyu Yeon Lee <rebel1324@gmail.com>
2019-02-21 12:57:20 +0900

    Crafting Attribute does not have Start Bonus anymore.

--- gamemodes/cityrp/plugins/crafting/attributes/sh_craft_generic.lua
+++ gamemodes/cityrp/plugins/crafting/attributes/sh_craft_generic.lua
@@ -1,2 +1,3 @@
 ATTRIBUTE.name = "Generic Crafting"
 ATTRIBUTE.desc = "Level of generic crafting."
+ATTRIBUTE.noStartBonus = true
--- gamemodes/cityrp/plugins/crafting/attributes/sh_craft_weapon.lua
+++ gamemodes/cityrp/plugins/crafting/attributes/sh_craft_weapon.lua
@@ -1,2 +1,3 @@
 ATTRIBUTE.name = "Weapon Crafting"
 ATTRIBUTE.desc = "Level of weapon crafting."
+ATTRIBUTE.noStartBonus = true
